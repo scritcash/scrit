60996537627ff410ab6b5eff10b523055b8e78e2
Frank Braun <frank@cryptogroup.net>
2019-11-05 22:25:43 +0000

    netconf: fix comments

--- netconf/errors.go
+++ netconf/errors.go
@@ -18,18 +18,18 @@ var ErrMGreaterN = errors.New("netconf: quorum M is greater than the number of m
 // number of mints N divided by 2.
 var ErrQuorumTooSmall = errors.New("netconf: quorum M too small (must be > n/2)")
 
-// ErrSignEpochStartNotBeforeEnd is returned when the signing epoch start is not
+// ErrSignEpochStartNotBeforeSignEnd is returned when the signing epoch start is not
 // before the end.
 var ErrSignEpochStartNotBeforeSignEnd = errors.New("netconf: signing epoch start is not before signing end")
 
-// ErrSignEpochEndNotBeforeValidate is returned when the signing epoch start is
+// ErrSignEpochEndNotBeforeValidateEnd is returned when the signing epoch start is
 // not before the end.
 var ErrSignEpochEndNotBeforeValidateEnd = errors.New("netconf: signing epoch end is not before end validate end")
 
-// ErrSignEpochWrongBoundaires is returned when the signing epoch boundaries
+// ErrSignEpochWrongBoundaries is returned when the signing epoch boundaries
 // do not match exactly.
 var ErrSignEpochWrongBoundaries = errors.New("netconf: signing epoch boundaries do not match exactly")
 
-// ErrValidationLongerThanNextSiging is returned when the validation period is
+// ErrValidationLongerThanNextSigning is returned when the validation period is
 // longer than the next signing epoch.
 var ErrValidationLongerThanNextSigning = errors.New("netconf: validation period is longer than next signing epoch")
