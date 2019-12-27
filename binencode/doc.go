9f091e4cd9167b5cb1dace105c91fbab0e7262fc
Frank Braun <frank@cryptogroup.net>
2019-12-27 13:50:09 +0000

    binencode: move documentation to doc.go

---
+++ binencode/doc.go
@@ -0,0 +1,6 @@
+/*
+Package binencode provides functions to encode values into length-encoded slices.
+
+It can be used to work on secure memory. Only int16, int32, int64 and []byte are supported. Inserting and skipping zero bytes is supported.
+*/
+package binencode
