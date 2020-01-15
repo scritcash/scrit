79a2575a6c504bfa11c8cbc49a1fdc16c4d1f32e
Frank Braun <frank@cryptogroup.net>
2020-01-15 22:18:48 +0000

    extend netconf.LoadFederation()

--- netconf/federation.go
+++ netconf/federation.go
@@ -1,6 +1,7 @@
 package netconf
 
 import (
+	"fmt"
 	"path/filepath"
 )
 
@@ -15,14 +16,34 @@ type Federation struct {
 func LoadFederation(dir string) (*Federation, error) {
 	var f Federation
 	filename := filepath.Join(dir, DefNetConfFile)
+	fmt.Printf("loading '%s'\n", filename)
 	n, err := LoadNetwork(filename)
 	if err != nil {
 		return nil, err
 	}
+	fmt.Printf("validate '%s'\n", filename)
 	if err := n.Validate(); err != nil {
 		return nil, err
 	}
 	f.network = n
+	f.mints = make(map[string]*Mint)
 
+	// TODO: the following is not correct in all situations, we have to validate
+	// in the current signing period (and maybe later ones)
+
+	mints := n.Mints()
+	for mn := range mints {
+		filename := filepath.Join(dir, DefMintDir, mn+".json")
+		fmt.Printf("loading '%s'\n", filename)
+		m, err := LoadMint(filename)
+		if err != nil {
+			return nil, err
+		}
+		fmt.Printf("validate '%s'\n", filename)
+		if err := m.Validate(); err != nil {
+			return nil, err
+		}
+		f.mints[mn] = m
+	}
 	return &f, nil
 }
